2e008a218c1f7fb7e4b315cdce6a98b3e8a89e2d
Moinul Hossain Nabil <nnaabiill@gmail.com>
2020-08-31 03:01:39 +0600

    changes

---
+++ build/precache-manifest.89e3d4b9ef564b1079d3ed32cdd9856b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5670ef4ea5210e01871217272b9335ce",
+    "url": "/e-commerce-cart-react/index.html"
+  },
+  {
+    "revision": "ac7451d9614a85a8a1fe",
+    "url": "/e-commerce-cart-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3d47197898ab1e03a896",
+    "url": "/e-commerce-cart-react/static/js/2.9acecf36.chunk.js"
+  },
+  {
+    "revision": "ef7f0128066dad638e14e7838f835a83",
+    "url": "/e-commerce-cart-react/static/js/2.9acecf36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac7451d9614a85a8a1fe",
+    "url": "/e-commerce-cart-react/static/js/main.833bb9a1.chunk.js"
+  },
+  {
+    "revision": "4ec8eda0b189d1ba2523",
+    "url": "/e-commerce-cart-react/static/js/runtime-main.3be84811.js"
+  }
+]);
