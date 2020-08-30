f0c93930132428f577eaa3191432dda7547d7ae1
Moinul Hossain Nabil <nnaabiill@gmail.com>
2020-08-31 01:32:21 +0600

    changes

---
+++ build/precache-manifest.e417f4ade6d756719b8f82d7dcfec1e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03180b49716f70dcc3e213616bf4d80a",
+    "url": "/https//:MoinulHossainNabil.github.io/e-commerce-cart-react/index.html"
+  },
+  {
+    "revision": "1e3c00257289cf46311a",
+    "url": "/https//:MoinulHossainNabil.github.io/e-commerce-cart-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3d47197898ab1e03a896",
+    "url": "/https//:MoinulHossainNabil.github.io/e-commerce-cart-react/static/js/2.9acecf36.chunk.js"
+  },
+  {
+    "revision": "ef7f0128066dad638e14e7838f835a83",
+    "url": "/https//:MoinulHossainNabil.github.io/e-commerce-cart-react/static/js/2.9acecf36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e3c00257289cf46311a",
+    "url": "/https//:MoinulHossainNabil.github.io/e-commerce-cart-react/static/js/main.f1c8843a.chunk.js"
+  },
+  {
+    "revision": "f48c62b9a11f6a7e3962",
+    "url": "/https//:MoinulHossainNabil.github.io/e-commerce-cart-react/static/js/runtime-main.7bc7b824.js"
+  }
+]);
