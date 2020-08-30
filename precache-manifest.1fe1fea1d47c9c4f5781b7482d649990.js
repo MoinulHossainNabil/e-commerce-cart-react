d69f410ce954c4fd57ebcc2ae7089fcd199a22ba
Moinul Hossain Nabil <nnaabiill@gmail.com>
2020-08-31 03:10:54 +0600

    Updates

--- precache-manifest.1fe1fea1d47c9c4f5781b7482d649990.js
+++ precache-manifest.1fe1fea1d47c9c4f5781b7482d649990.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5670ef4ea5210e01871217272b9335ce",
+    "revision": "612c463e09ce11914c4472f474e86961",
     "url": "/e-commerce-cart-react/index.html"
   },
   {
-    "revision": "ac7451d9614a85a8a1fe",
+    "revision": "d28ae4220d26d9615e8d",
     "url": "/e-commerce-cart-react/static/css/main.5f361e03.chunk.css"
   },
   {
-    "revision": "3d47197898ab1e03a896",
-    "url": "/e-commerce-cart-react/static/js/2.9acecf36.chunk.js"
+    "revision": "ca2dcbac2a0840e8e75f",
+    "url": "/e-commerce-cart-react/static/js/2.cc5a2ac5.chunk.js"
   },
   {
     "revision": "ef7f0128066dad638e14e7838f835a83",
-    "url": "/e-commerce-cart-react/static/js/2.9acecf36.chunk.js.LICENSE.txt"
+    "url": "/e-commerce-cart-react/static/js/2.cc5a2ac5.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "ac7451d9614a85a8a1fe",
-    "url": "/e-commerce-cart-react/static/js/main.833bb9a1.chunk.js"
+    "revision": "d28ae4220d26d9615e8d",
+    "url": "/e-commerce-cart-react/static/js/main.6c09c8cf.chunk.js"
   },
   {
     "revision": "4ec8eda0b189d1ba2523",
